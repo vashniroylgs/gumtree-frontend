c858636ba28f566b2d08bb4a56832178c2435ac5
Saicharan-LGS <gangula.saicharan@labyrinthglobalsolutions.com>
2023-09-20 17:44:01 +0530

    edited home page

--- src/Components/HomePage/index.js
+++ src/Components/HomePage/index.js
@@ -5,21 +5,25 @@ import "./index.css";
 const HomePage = () => {
   return (
     <>
-      <div className="home-main-contianer">
-        <div className="home-mini-all-categories-contianer">
+      <div className="home-main-contianer" style={{"width":"100vw"}}>
+        <div className="home-mini-all-categories-contianer" style={{"width":"75%"}}>
           <FeaturedCategories />
           <DiscoveredFinds />
         </div>
-        
-        <div className="ad-container1">
-          
-          </div>
+        <div className="home-main-banner">
+          <img className="banner-image-home" src="https://res.cloudinary.com/dx0tk0a56/image/upload/v1694503153/Screenshot_920_lheb8k.png" alt=""/>
+          <img className="banner-image-home" src="https://res.cloudinary.com/dx0tk0a56/image/upload/v1694503153/Screenshot_920_lheb8k.png" alt=""/>
+          <img className="banner-image-home" src="https://res.cloudinary.com/dx0tk0a56/image/upload/v1694503153/Screenshot_920_lheb8k.png" alt=""/>
+        </div>
+        {/*<div  style={{"width":"20%","display":"flex","flexDirection":"column"}}>
+        <div className="ad-container1" style={{"width":"100%"}}>
+        </div>
           <div className="ad-container2">
-          
           </div>
           <div className="ad-container3">
           
           </div>
+  </div>*/}
       </div>
     </>
   );
